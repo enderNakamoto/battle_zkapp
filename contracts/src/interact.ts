b92cd26965b3d5f78fd08a8d7df662dbf7a9a37c
enderNakamoto <ender.nakamoto@gmail.com>
2024-09-08 05:49:38 -0700

    commenting out interact for now

--- contracts/src/interact.ts
+++ contracts/src/interact.ts
@@ -1,41 +1,41 @@
 import { Mina, NetworkId, PrivateKey } from 'o1js';
-import { Add } from './Add';
+import { Battle } from './Battle';
 
-let feepayerKey = PrivateKey.fromBase58('EKEXnEq52gL3aQDZ9W6gk8CcGTxViHhaG2MjrigUq9HrDQ2C2JNh');
-let zkAppKey = PrivateKey.fromBase58('EKDoyF5QvUPZsdHXdGHWVgWzoeCtVJ1KEbLNM9ZUHondnHi1f76h');
+// let feepayerKey = PrivateKey.fromBase58('EKEXnEq52gL3aQDZ9W6gk8CcGTxViHhaG2MjrigUq9HrDQ2C2JNh');
+// let zkAppKey = PrivateKey.fromBase58('EKDoyF5QvUPZsdHXdGHWVgWzoeCtVJ1KEbLNM9ZUHondnHi1f76h');
 
 
-const Network = Mina.Network({
-  networkId: 'testnet' as NetworkId,
-  mina: 'https://api.minascan.io/node/devnet/v1/graphql',
-});
+// const Network = Mina.Network({
+//   networkId: 'testnet' as NetworkId,
+//   mina: 'https://api.minascan.io/node/devnet/v1/graphql',
+// });
 
-const fee = Number(0.1) * 1e9; // in nanomina (1 billion = 1.0 mina)
-Mina.setActiveInstance(Network);
-let feepayerAddress = feepayerKey.toPublicKey();
-let zkAppAddress = zkAppKey.toPublicKey();
-let zkApp = new Add(zkAppAddress);
+// const fee = Number(0.1) * 1e9; // in nanomina (1 billion = 1.0 mina)
+// Mina.setActiveInstance(Network);
+// let feepayerAddress = feepayerKey.toPublicKey();
+// let zkAppAddress = zkAppKey.toPublicKey();
+// let zkApp = new Add(zkAppAddress);
 
-// compile the contract to create prover keys
-console.log('compile the contract...');
-await Add.compile();
+// // compile the contract to create prover keys
+// console.log('compile the contract...');
+// await Add.compile();
 
-try {
-  // call update() and send transaction
-  console.log('build transaction and create proof...');
-  let tx = await Mina.transaction(
-    { sender: feepayerAddress, fee },
-    async () => {
-      await zkApp.update();
-    }
-  );
-  await tx.prove();
+// try {
+//   // call update() and send transaction
+//   console.log('build transaction and create proof...');
+//   let tx = await Mina.transaction(
+//     { sender: feepayerAddress, fee },
+//     async () => {
+//       await zkApp.update();
+//     }
+//   );
+//   await tx.prove();
 
-  console.log('send transaction...');
-  const sentTx = await tx.sign([feepayerKey]).send();
-  if (sentTx.status === 'pending') {
-    console.log('pending.....');
-  }
-} catch (err) {
-  console.log(err);
-}
+//   console.log('send transaction...');
+//   const sentTx = await tx.sign([feepayerKey]).send();
+//   if (sentTx.status === 'pending') {
+//     console.log('pending.....');
+//   }
+// } catch (err) {
+//   console.log(err);
+// }
